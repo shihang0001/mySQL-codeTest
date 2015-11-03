b7b745376ae486f93ba5c5ec0669f6cc2914b3bc
shihang0001 <649161851@qq.com>
2015-11-03 10:30:24 +0800

    add cont back

--- sql.class-10.28.sql
+++ sql.class-10.28.sql
@@ -22,6 +22,93 @@ create table employees(
        mysql> SELECT t1
            -> FROM test_timestamp;
 
+student
+recoster
+suppose physic 403
+roomasignmnet
+mistakes;
+physic canceled;
+weak ago;
+check NUm;
+partment settle street;
+exlain-> semester collodge
+few in class->nes semester;
+sign 403 cancel->list 10-> show the pehycisal call->decision->mail address
+
+envirinmont
+loss habitat; grow cityreduce alarge habitat;
+longdestance;
+animal habitat loss: homming bird long destance->back porward;
+whole area habitat feed sleep bits swings;/minues
+drink flower all across body fat paln habitat ;
+opposite true hummingbird flower exists''
+hot spot agriculture for instance ;
+nesting human activities;
+puputation'claen up repalantting native tours hbit visit humming bird number grow'
+echological littke habit tat protect breddingfind the prevent population
+learn more abanding acract infact coming student more information
+international databses;
+less look's' like perfect probuleng attractionnumber ;
+back repoool
+migration less spend habit some hummit using certian root fing the less number
+countrys->habitat hummingbird;
+
+
+film hastory astablish ;
+certa haibie ;
+fit categronws;
+film maker->90;
+framch JPLW;
+02->28 20->certy twenty 30 unit habit using confusing science;->more fictional;
+alement scients;
+uderwaters animal world cretaure;
+ji,am creature;
+human using donw water using music humanbeeans;
+change the ninit;
+unlike huamnas;
+the way miss up ;huamn animals;
+certin what seeaj
+familiar-
+normal
+unuaual;
+meals;'
+great celebrate '
+movie under water job stopsemeplarity;
+show cwojsohishang;
+hit attcks;
+equpment
+as nit opent sead;
+show main was evisigate;
+ficion;nauture good attack konew them general public expectation konw  heahly recpaty
+shihansghihihisnghsi
+
+
+procrsor;
+laete loss computormail box yesterday;'
+this week'
+email keep out ollocy;
+dont 'd 'ever need shiangshihsnghihsan;
+keitchen locyey;styles;
+cookies cake ask couer as nothing party timing help with poloct;
+glory knowledge mention writing about fild like wat versable;
+influence;
+america specisis;
+form same seaksdes
+collective spend orry project middle age;
+nintycentury realizement location;
+well make plamin success commercial
+new around cetury most imoprtant connectied;
+back workd logicly before;
+show detail dte=int x=ss;
+return back details show ing thishig ;
+
+show detal;of tables;
+
+
+
+INSERT into products value();
+
+
 
 
            mysql> CREATE TABLE tbl_name(name varchar(10),
@@ -66,6 +153,8 @@ mysql> select *from products;
 
 
 
+
+INSERT INTO priducts value()
 
 
 
