0a0ff13cfe25f2a4cf6adf4c5387a6bcfefa768b
shihang0001 <649161851@qq.com>
2015-11-16 02:06:27 +0800

    add fix method class

--- sql.class-10.28.sql
+++ sql.class-10.28.sql
@@ -22,7 +22,7 @@ insert int
        mysql> SELECT t1
            -> FROM test_timestamp;
 
-sdsad
+sdsadwww
 imoprt init shoq select into shihangshih;
 init value show databaes;
 show detials of the databses;
