7ca352d169071a883602a906cbb77e4e969459b9
shihang0001 <649161851@qq.com>
2015-11-21 17:15:21 +0800

    add fix method class

--- sql.class-10.28.sql
+++ sql.class-10.28.sql
@@ -21,7 +21,7 @@ create table employees(
 insert int
        mysql> SELECT t1
            -> FROM test_timestamp;
-
+lll
 sdsadwwwzzz
 imoprt init shoq select into shihangshih;
 init value show databaes;
