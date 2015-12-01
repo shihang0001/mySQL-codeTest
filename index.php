bbc4c2fca2308f521c87d7224fcd0671fe14748c
shihang0001 <649161851@qq.com>
2015-12-02 00:24:21 +0800

    add fix method class

---
+++ index.php
@@ -0,0 +1,214 @@
+<?php
+	$str_browser_language = !empty($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? strtok(strip_tags($_SERVER['HTTP_ACCEPT_LANGUAGE']), ',') : '';
+	$str_browser_language = !empty($_GET['language']) ? $_GET['language'] : $str_browser_language;
+	switch (substr($str_browser_language, 0,2))
+	{
+		case 'de':
+			$str_language = 'de';
+			break;
+		case 'en':
+			$str_language = 'en';
+			break;
+		default:
+			$str_language = 'en';
+	}
+
+	$arr_available_languages = array();
+	$arr_available_languages[] = array('str_name' => 'English', 'str_token' => 'en');
+	$arr_available_languages[] = array('str_name' => 'Deutsch', 'str_token' => 'de');
+
+	$str_available_languages = (string) '';
+	foreach ($arr_available_languages as $arr_language)
+	{
+		if ($arr_language['str_token'] !== $str_language)
+		{
+			$str_available_languages .= '<a href="'.strip_tags($_SERVER['PHP_SELF']).'?language='.$arr_language['str_token'].'" lang="'.$arr_language['str_token'].'" xml:lang="'.$arr_language['str_token'].'" hreflang="'.$arr_language['str_token'].'">'.$arr_language['str_name'].'</a> | ';
+		}
+	}
+	$str_available_languages = substr($str_available_languages, 0, -3);
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head lang="<?php echo $str_language; ?>" xml:lang="<?php echo $str_language; ?>">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>MAMP PRO</title>
+
+<!-- <python>
+
+from urilib.request mport urlopen
+from urllib.error imoprt HTTPError
+from bs4 import beautifulsoup
+ def getTitle(url):
+     try:
+		     html =erlopen(url)
+     except HTTPError a e:
+		     return none
+		try:
+		     bsObj =Beautidulsoup(html.read())
+				 title =bsObj.body.<h1></h1>
+		except AttributeError as e:
+		     return none
+		return Title
+	title=getTitle("http://www.codecademy.com/exercise/dif.html.pdf")
+	if title==none:
+	      print("Title could not be found ")
+	else:
+	   print(title)
+</python> -->
+
+<!--
+<inis how >
+
+intergraded serveice
+  ip internet ;
+	do show no indequrie  application no Qos sippoert '
+	'
+imoprt access serceide bsidenm
+da;
+from uneil  linek show line show imoprtnat inis howle ;
+dou spsoup bestdurl soulple ;eg dte tiyel shoe line baeacasese;
+init showmline believe he tey : inis how line;
+try : html=errlosdni shlinishwoe lineinger ;
+skne show line between belloe wbeltwenn ineolinely sojw ;ii e shwl shiwline shiw limkd ehsow dekt beode;
+
+linnish wohos wline sow ldsjadareate application many on;
+schedule pass from more buffer pass service he familly for transimistion ;
+traffic with signmaney deylay whe requirement shw can the  be service ahead of more yelerenta nire in real time datba be;
+best more offer beste cacity wieleel bettre by acb kead tis teamnekshie;
+ingress route (at the edge of the network)
+the ingress will determine s the perhot
+inishwor e leoe ni snwhiew ine hiw h ni show da atvasae s;
+kinis hin shw inek kinl shiwninis hline detabsbase;
+line sefeiceide ;
+sii  show line preductoive ruby inidlshiw line hisle line shiw line giwsh;;;
+inits howl lien because id the deirline ;
+ pirnt lineh("inits hw lie inits linearnlyonusdhi;
+ ;
+ else print link inisgwi")
+
+<init></init> -->
+
+<!-- <python>
+init show line data link show line coonection->python;
+array()=sstr();
+$arr-\aeer(nishwo (str=tru-/init line uril triltest())
+from urilib.request import erlopen
+from urllib.error import HTTPError
+from bs4 import beautifulsoup
+      def grtTittle(url):
+			    try: html- erlopen(url)
+					    except HTTPError e
+							   return none;
+
+					rey:
+
+					bsOBjline=beautifulspoup(html.read())
+					title= bsObj.body.h1;
+					except grtttitle (unic libe shople->init line show data baes;)
+import urllib.from urllib.request()
+import urllib2.request().
+
+cold start condition always sstheresure init show line
+trachote value init show increase the packet linear poit 3acks
+back forward the init line of the ack back foward init show
+
+
+case 'en'
+    is lie show lable'init'
+    avialable;
+		urllib2.upload()
+		content=except/pect line;
+		requestTest()
+		urllib2.upload road (url)
+		urllib2Test(url);
+	input inist line show dataline
+
+
+</python> -->
+<!--
+<python list-33>
+
+
+
+<python> -->
+
+
+
+
+<style type="text/css">
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .9em;
+        color: #000000;
+        background-color: #FFFFFF;
+        margin: 0;
+        padding: 10px 20px 20px 20px;
+    }
+
+    samp {
+        font-size: 1.3em;
+    }
+
+    a {
+        color: #000000;
+        background-color: #FFFFFF;
+    }
+
+    sup a {
+        text-decoration: none;
+    }
+
+    hr {
+        margin-left: 90px;
+        height: 1px;
+        color: #000000;
+        background-color: #000000;
+        border: none;
+    }
+
+    #logo {
+        margin-bottom: 10px;
+        margin-left: 28px;
+    }
+
+    .text {
+        width: 80%;
+        margin-left: 90px;
+        line-height: 140%;
+    }
+</style>
+</head>
+
+<body>
+    <p><img src="MAMP-PRO-Logo.png" id="logo" alt="MAMP PRO Logo" width="250" height="49" /></p>
+
+<?php if ($str_language == 'de'): ?>
+
+    <p class="text"><strong>Der virtuelle <span lang="en" xml:lang="en">Host</span> wurde erfolgreich eingerichtet.</strong></p>
+    <p class="text">Wenn Sie diese Seite sehen, dann bedeutet dies, dass der neue virtuelle <span lang="en" xml:lang="en">Host</span> erfolgreich eingerichtet wurde. Sie können jetzt Ihren <span lang="en" xml:lang="en">Web</span>-Inhalt hinzufügen, diese Platzhalter-Seite<sup><a href="#footnote_1">1</a></sup> sollten Sie ersetzen <abbr title="beziehungsweise">bzw.</abbr> löschen.</p>
+    <p class="text">
+        Server-Name: <samp><?php echo $_SERVER['SERVER_NAME']; ?></samp><br />
+        Document-Root: <samp><?php echo $_SERVER['DOCUMENT_ROOT']; ?></samp>
+    </p>
+    <p class="text" id="footnote_1"><small><sup>1</sup> Dateien: <samp>index.php</samp> und <samp>MAMP-PRO-Logo.png</samp></small></p>
+    <hr />
+    <p class="text">This page in: <?php echo $str_available_languages; ?></p>
+
+<?php elseif ($str_language == 'en'): ?>
+
+    <p class="text"><strong>The virtual host was set up successfully.</strong></p>
+    <p class="text">If you can see this page, your new virtual host was set up successfully. Now, web content can be added and this placeholder page<sup><a href="#footnote_1">1</a></sup> should be replaced or deleted.</p>
+    <p class="text">
+        Server name: <samp><?php echo $_SERVER['SERVER_NAME']; ?></samp><br />
+        Document root: <samp><?php echo $_SERVER['DOCUMENT_ROOT']; ?></samp>
+    </p>
+    <p class="text" id="footnote_1"><small><sup>1</sup> Files: <samp>index.php</samp> and <samp>MAMP-PRO-Logo.png</samp></small></p>
+    <hr />
+    <p class="text">Diese Seite auf: <?php echo $str_available_languages; ?></p>
+
+<?php endif; ?>
+
+</body>
+</html>
