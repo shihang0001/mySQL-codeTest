a782bce67433230abb49588c49beedda435bc4db
shihang <649161851@qq.com>
2016-01-13 11:37:58 +0800

    add func of js

---
+++ review.sql
@@ -0,0 +1,271 @@
+create databases [if not exists] databases_time;
+show databases;
+use databases_time;
+drop database [if exists] database_name;
+
+create table tbl_nam(
+    create on timestep default 0;
+      changed on timestep default current_timestep;
+);
+
+create table tavlename(
+      emplyee int(10) not null,
+      lastname char(50) not null,
+      reportto int(11) default null,
+      Primary key (emplyee number)
+);
+
+CREATE TABLE table()
+
+create table time ('t1' TIMESTAMP);
+set time_zone='+00:00';
+insert into time value('2001 01:00');
+select t1 from time;
+
+
+set time_zone='03:00';
+select t1 from test_timttable;
+
+show tables;
+describe table_name;
+describe emplyee;
+
+alter(colum) table table_name[,option...]
+options:  add[column] create defination first after colnumane;
+
+drop primary key ;
+drop index index_name rename[to] new table_name;
+ordered by coloumn_name;
+table_option;
+
+Drop [temporary] table [is exist] table_name[, table_name];
+
+trunvcate table table_name;
+
+create [unique full text special index index_name]
+  using[bst hash| rtree];
+
+  drop index index_name on table_name;
+  select *from tables where condition
+  frup by group having grup_conditions
+  order by sort_colimns
+  limit limits;
+  select *from emplyee;
+  select lastname, firstname,FROM emplyee;
+
+  select firstname,last name,e-mail from emplees where jobtitle="president"
+  select distinct jobtitle from emplyee;
+  select *from limited 10,5;
+  select office city phone
+  from office where country using or counrty frounce ;
+  where country in (usa, france)
+  selct column_list from table_name where tavle_name;
+  where column between lower_range and upper_range and where column_1=low range and clolumn1<=upper_range
+  select from buy from products where buyprice not between 20 and 40
+  select productcode,productname from products where productcode like "name"
+  select from name union select emplyee is first nae from emplyee;
+
+  select column_list from t1 inner join t2 on join_condition1;
+  inner join t1 on joi_conditions2
+  where where_conditions;
+  selecr a product from products a inner join orderdetail b on a.productcode=b.productscode;
+  select c.customer , customername, o from suctomerc
+  left join order roder on customer=o.customer;
+  where order_number is null
+  select column_name, colomun_lest function(expression)
+  from table where where_conditions
+  group by col_1, colon _2
+  order by column_list
+
+  select status from order group by status select status count(*)
+  from orders group by status
+
+  insert into clssroom value)city,)
+
+    insert into temple select *from office where counrty ='us'
+    insertt into productlist set products time='lucy car'
+
+    update emplyee(table_name)
+      set email='email_list neme';
+      where emplyee=1
+
+  delet from emplyee,office where iffice = 4
+  delete from emplyee where emplyee.office=office,office an where office.offcidetime=10;
+
+    current_timestep;
+
+
+
+crete produce (var lune inie char (1092))
+begin declear i int dufsuly from int pc init en where set str =i+i;
+if pc()
+end str =outside
+delimiter create produce getallprudct()
+begiin select *from products;
+delemiter
+call fetproduct()
+seclear total_time intnit select *from total_products from products;
+mode para_name param_type(param_size)
+
+set () call get information()
+select $$from str;
+
+if expression then comands
+end if;
+if expression then commands
+elseif expression then commands
+else command end if;
+
+case when expression then commands
+     when expression then commandselse commandsend case
+
+  delemiter $$
+  drop produce if exist whilelooppro$$
+  create produce whileloop proc()
+        begin
+            declear *from int;
+            delcear str varchar(255)
+            set h--1;
+            where set str='';
+            while *from set str concat ('set');
+            end while select from str end$$
+            delemiter;
+
+
+  create procedure id exist from process
+    create produce reaploppproc()
+      begin from seclear str varchar(255)
+      set ==1;
+      repeat end repearend iniw select from str end $$;
+      deleceyer;
+
+drop produce is exist loop
+creat produce is exist loopprocess
+create producter loooper()
+    begin
+       declear *from int set ==1;
+       set str =''
+       lopp_lapbe :loop
+            if from each begin changdown bom(end)
+
+  create trigger trigger_name_trigger event
+    on table_name
+    for each row begin
+    end
+    select *from information_scheme trigger
+    where trigger scheme="datacases_name" and event_object _table="table_name";
+    drop trigger table_name.trigger_name_trigger.
+
+    create alview datbases_name.view as select statement
+
+      create view vwproducr as select productcode,
+        producter,productname
+        from products where buyprice>(
+        select ag (buyprice)
+        from products
+      )order by buyprice desc;
+
+
+  show create ciew(databame_name)[view_name];
+
+  alter (where init databases_name view_name)
+  as    [select statement]
+
+  drop view if exists organizarion
+
+create table'product'(
+         product varchar(15) not null;
+         product_code varchar (11)not null;
+         primary key(product);
+         fulltext("products")
+)
+
+alter table'class_name'
+drop index 'productdescription';
+
+select productname. productline
+from productdescription where match(prodcut_name) against ('trunk_pick' in boolean mode)
+
+
+substring(str, pos)
+substring(str from pos);
+
+substring(str, pos,len)
+substring(str from pos for len)
+
+select substring("mysqk" substring,-9)
+
+select concat(a,v,v,b,b,)
+select concatus()from customers limit 5
+update tbl_name
+  set firld_name=replace(file_name,string_to_find, string_to_replace)
+  where conditions
+
+  update products set prudct_description=replace(productdescription,"about","about+")
+
+    select productcode, sum(a&b) total from orderdetail
+    group by prudctscode
+
+    select AVG(buyprice) average_buy_price
+    from procuts;
+    select max(buy proce)high_price
+        min(buy_price) lower_price
+        from products
+        select count(*)as total from products;
+
+        if(expr,if_true,if_false_expr)
+
+        select customernumber,customerid
+        if (state is null, '')n/1state ,country from cuatomers;
+
+create user user indetified by password
+  CREATE USER user indentified by password
+  CREATE USER 'dmadmin' @locohoast indeitided by carefied_user;
+  CREATE user "superadmin@locaohaost" indentified by securied;
+  CREATE USER 'mysql_admin_mysql.org' INDETIFIED 'securitypass704';
+
+  indert into user(hostmusermpassword)
+  value (localhost,dmadmin,password(crEate_user));
+
+  user mysql;
+  UPDATE user
+  SET password-PASSWORD("707195")
+  where user='mysql_tutorial'and host="mysql_org.org"
+
+  flush privileges;
+  select passworf from ('selected 1970')as encrypt_password.
+  set /select password for mysql()mysql.org=PASSWORD('select1970').
+
+  GRABT usage on to mysql@mysql tutorial.org identify by 'select 1970'
+
+  GRANT privilefe(column_list)
+  on [object_type] priviledege_level
+
+to account [indentified by 'password']
+[request+encyption] with_options
+
+CREATE USER 'super@userlocalhost' indentified by securite2
+CREATE user indetified by "
+grant all classimodel to super @witth model"
+  init show select grant update delected delete on "classmate  iint"
+  init show linek
+
+
+  create init showd databases;
+    create table cretae
+      create uni view delede frop event exevute int show link grant k option;
+        index inset indesy locak table process;
+        procy select from showd atadbsesl
+        trigger show cirw shuut down trigger usege prociage link
+        usege ni revoke update delete on 'calss 'from localshot@init link;
+        show index from emplyee
+        show alter table empleyee
+        show index from emplyee
+        analyse sysatable inie emloeyee;
+        op[timize] table iinit show tanle inie;
+        show datbales;
+        show table inir table_table name;
+        chexk table remote repair table table_name
+
+
+function get uner($databses;)
